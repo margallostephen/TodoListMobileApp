fe039fec44668408db1f2e8c79cfae66d4c53079
margallostephen <spmargallo@gmail.com>
2023-05-25 20:49:17 +0800

    Add welcome label and task count

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/app_bar.dart';
+import 'package:hive/hive.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,8 +10,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  dynamic arguments;
+  String? userKey;
+  List<Map<String, dynamic>> taskData = [];
+  final tasks = Hive.box('tasks');
+
+  @override
+  initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() {
+    final data = tasks.keys.map((key) {
+      final item = tasks.get(key);
+      return {
+        'key': key,
+        'name': item['name'],
+        'description': item['description'],
+        'date': item['date'],
+        'userKey': item['userKey'],
+      };
+    }).toList();
+    setState(() {
+      taskData = data.reversed.toList();
+    });
+  }
+
+  int countCurrentUserTask() {
+    int count = 0;
+    for (var i = 0; i < taskData.length; i++) {
+      if (taskData[i]['userKey'] == userKey) {
+        count++;
+      }
+    }
+    return count;
+  }
+
   @override
   Widget build(BuildContext context) {
+    arguments = ModalRoute.of(context)!.settings.arguments;
+    userKey = arguments['userKey'];
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 54, 23, 94),
       body: Padding(
@@ -26,6 +66,34 @@ class _HomeState extends State<Home> {
             ),
             const SizedBox(
               height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Welcome ${arguments['name']}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Task Count: ${countCurrentUserTask()}',
+                      style: const TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
