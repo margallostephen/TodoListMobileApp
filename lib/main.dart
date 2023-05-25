63b8cf7df0def6bf63f66fdcdc009924f0e5c623
cedrixmonreal <cedmonreal14@gmail.com>
2023-05-25 23:01:26 +0800

    Finalize task form

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'screens/register.dart';
 import 'screens/login.dart';
+import 'screens/task_form.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +23,7 @@ class TodoList extends StatelessWidget {
         routes: {
           '/login': (context) => const Login(),
           '/register': (context) => const Register(),
+          '/task_form': (context) => const TaskForm(),
         },
       ),
     );
