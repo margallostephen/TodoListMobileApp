d44f4f0948c82be155b2291f463cc3f1b301fd35
cedrixmonreal <cedmonreal14@gmail.com>
2023-05-25 21:49:15 +0800

    Add input fields

--- lib/screens/task-form.dart
+++ lib/screens/task-form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../components/app_bar.dart';
+import '../components/style.dart';
 
 class TaskForm extends StatefulWidget {
   const TaskForm({super.key});
@@ -47,6 +48,73 @@ class _TaskFormState extends State<TaskForm> {
               ),
               const SizedBox(
                 height: 20,
+              ),
+              Form(
+                key: taskFormKey,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  padding: const EdgeInsets.fromLTRB(20, 25, 20, 25),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: nameController,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Style.violet,
+                          ),
+                          labelText: 'Name',
+                          prefixIcon: Icon(Icons.task),
+                          prefixIconColor: Style.violet,
+                          border: Style.normal,
+                          enabledBorder: Style.normal,
+                          focusedBorder: Style.focused,
+                          focusedErrorBorder: Style.errorFocused,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: descriptionController,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Style.violet,
+                          ),
+                          labelText: 'Description',
+                          prefixIcon: Icon(Icons.description),
+                          prefixIconColor: Style.violet,
+                          border: Style.normal,
+                          enabledBorder: Style.normal,
+                          focusedBorder: Style.focused,
+                          focusedErrorBorder: Style.errorFocused,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        controller: dateController,
+                        decoration: const InputDecoration(
+                          labelStyle: TextStyle(
+                            color: Style.violet,
+                          ),
+                          labelText: 'Date',
+                          prefixIcon: Icon(Icons.calendar_month_outlined),
+                          prefixIconColor: Style.violet,
+                          border: Style.normal,
+                          enabledBorder: Style.normal,
+                          focusedBorder: Style.focused,
+                          focusedErrorBorder: Style.errorFocused,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
